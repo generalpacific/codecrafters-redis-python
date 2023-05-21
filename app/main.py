e7191547961a9c1bc537e19357e03f88eee59e39
generalpacific <prash.c.29@gmail.com>
2023-05-21 08:47:18 -0700

    Implement command parsing returning repsonse for ping command

--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
 import socket
 
+BUFFER_SIZE = 100
+
+
+def __decode_received_msg(msg):
+    print("Decoding: " + str(msg))
+
+    msg_str = msg.decode()
+    print("Msg str: " + msg_str)
+
+    if "ping" in msg_str:
+        return "ping"
+    else:
+        return "unknown_command"
+
+
+def __encode_ping_response():
+    response = "+PONG\r\n"
+    return response.encode("utf-8")
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-   
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    (conn, address) = server_socket.accept()  # wait for client
+
+    print("Received conn from: " + str(address))
+
+    msg = conn.recv(BUFFER_SIZE)
+    print("Received " + str(msg) + " type: " + str(type(msg)) + " from " + str(address))
+
+    command = __decode_received_msg(msg)
+
+    if command == "ping":
+        conn.send(__encode_ping_response())
+    else:
+        print("unknown command closing connection.")
 
 
 if __name__ == "__main__":
