0d760c2560b4e12fe33d79f76a29eeb5463f36bd
generalpacific <prash.c.29@gmail.com>
2024-01-07 19:49:30 -0800

    Implement using EventLoop based asynio network server

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
-import socketserver
+import asyncio
 
-BUFFER_SIZE = 100
+BUFFER_SIZE = 1024
 
 
-class RedisHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(BUFFER_SIZE).strip()
-        # We presume the client will only send 'PING'
-        print("Received request: " + str(self.data))
-        self.request.sendall(b'+PONG\r\n')
+async def handle_client(reader, writer):
+    while True:
+        request = await reader.read(BUFFER_SIZE)
+        print("Got request: " + str(request))
+        if not request:
+            break
+        writer.write("+PONG\r\n".encode())
+        await writer.drain()
+    writer.close()
+
+
+async def start_server(host, port):
+    srv = await asyncio.start_server(
+        handle_client, host, port)
+    async with srv:
+        await srv.serve_forever()
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    socketserver.ThreadingTCPServer.allow_reuse_address = True
-    with socketserver.ThreadingTCPServer(("localhost", 6379), RedisHandler) as server:
-        server.serve_forever()
+    asyncio.run(start_server("localhost", 6379))
 
 
 if __name__ == "__main__":
