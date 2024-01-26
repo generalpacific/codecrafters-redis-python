0d1f61800f834667cb53d78b6a1b2aaa810cedb9
generalpacific <prash.c.29@gmail.com>
2024-01-25 18:59:55 -0800

    Implement ttls

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 from enum import Enum
 
 BUFFER_SIZE = 1024
@@ -29,9 +30,15 @@ async def decode_echo(request):
 async def decode_set(request):
     parts = request.split("\r\n")
     size = await decode_size_array(parts[0])
+    print("parts: ")
+    for part in parts:
+        print(part)
     if size == 1:
         return "".encode()
-    return [parts[4], parts[6]]
+    if len(parts) > 7:
+        return [parts[4], parts[6], datetime.datetime.now() + datetime.timedelta(milliseconds=int(parts[10]))]
+    else:
+        return [parts[4], parts[6], datetime.datetime.max]
 
 
 async def decode_get(request):
@@ -70,12 +77,18 @@ async def handle_client(reader, writer):
             writer.write(b"$" + str(len(argument)).encode() + b'\r\n' + argument + b"\r\n")
         elif command is RedisCommand.SET:
             arguments = await decode_set(request)
-            IN_MEM_DATABASE[arguments[0]] = arguments[1]
+            IN_MEM_DATABASE[arguments[0]] = (arguments[1], arguments[2])
             writer.write(b'$2\r\nOK\r\n')
         elif command is RedisCommand.GET:
             argument = await decode_get(request)
-            return_value = IN_MEM_DATABASE[argument]
-            writer.write(b"$" + str(len(return_value)).encode() + b'\r\n' + return_value.encode() + b"\r\n")
+            db_value = IN_MEM_DATABASE[argument]
+            ttl = db_value[1]
+            return_value = db_value[0]
+            if ttl >= datetime.datetime.now():
+                writer.write(b"$" + str(len(return_value)).encode() + b'\r\n' + return_value.encode() + b"\r\n")
+            else:
+                writer.write(b"$-1\r\n")
+
         else:
             writer.write("+UNKNOWN\r\n".encode())
         await writer.drain()
